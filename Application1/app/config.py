39e77a94907c051cea7a85e65481a47caf3c57e0
Sai Deshmukh <58202919+dsai160898@users.noreply.github.com>
2020-09-18 12:33:56 +0530

    Update config.py

--- Application1/app/config.py
+++ Application1/app/config.py
@@ -1,7 +1,7 @@
 
 Config = {
     'darkskykey': '7e3b01715b0fa58d88eb9f8dc2ce94e1',
-    'googlekey' :"AIzaSyBRfiWKzBZT0ujxhXjyLvhoRlmwjCaVoJY"
+    'googlekey' :"AIzaSyB2OauQvIUxxEequvInYsXFB_D_NhsRvGw"
 }
 
 
